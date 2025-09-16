c852159a5b5f287af768f5a80d72065748e17787
root <root@ip-172-31-37-205.ap-south-1.compute.internal>
2025-09-16 21:21:40 +0000

    new terraform code with data source

--- terraform/main.tf
+++ terraform/main.tf
@@ -90,14 +90,3 @@ resource "aws_instance" "app" {
               EOF
 }
 
-# --- Outputs ---
-output "ec2_public_ip" {
-  description = "Public IP of EC2 instance"
-  value       = aws_instance.app.public_ip
-}
-
-output "ec2_public_dns" {
-  description = "Public DNS of EC2 instance"
-  value       = aws_instance.app.public_dns
-}
-
